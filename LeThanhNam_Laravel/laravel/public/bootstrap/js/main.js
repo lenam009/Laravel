8dcb32bb6ffd08eeeb29f085187c506c90746c67
lenam009 <144312524+lenam009@users.noreply.github.com>
2023-10-03 13:44:17 +0700

    laravel_update_2

---
+++ LeThanhNam_Laravel/laravel/public/bootstrap/js/main.js
@@ -0,0 +1,9 @@
+var dropdown = document.querySelector('.dropdown')
+dropdown.onmouseover = function (e) {
+    dropdown.style.visibility = "visible";
+}
+dropdown.onmouseout = function () {
+    dropdown.style.visibility = "hidden";
+}
+
+
